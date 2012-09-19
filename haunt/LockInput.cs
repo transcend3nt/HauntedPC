b18594653ec6a37086b6c4f7c3a7ff0c2e9de7e8
Clemi <hauntedpc>
2012-09-19 20:33:26 +0800

    Initial upload of project
    includes files needed to open project
    
    developed on visual studio 2008

---
+++ haunt/LockInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace haunt
+{
+    class LockInput
+    {
+        [DllImport("user32.dll")]
+        private static extern bool BlockInput(bool fBlockIt);
+
+        public static void Start()
+        {
+            while (true)
+            {
+                BlockInput(true);
+
+                // lock for 10 seconds
+                Thread.Sleep(10000);
+
+                BlockInput(false);
+
+                // free for 50 seconds
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
